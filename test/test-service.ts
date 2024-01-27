d16039d09cd2d0c50ae274b0cef6a40634f664f2
Tim Zadorozhny <tzador@gmail.com>
2024-01-27 20:12:53 +0100

    test

--- test/test-service.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const add = z
-  .function()
-  .args(z.object({ a: z.number(), b: z.number() }))
-  .returns(z.number())
-  .implement((args) => {
-    return args.a + args.b;
-  });
